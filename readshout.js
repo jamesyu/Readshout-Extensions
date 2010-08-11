baae7fdfe662033958cb5bb187711f74930cfd05
James Yu <james@scribd.com>
2010-08-11 01:04:04 -0700

    chrome extension first commit

---
+++ readshout.js
@@ -0,0 +1,27 @@
+(function() {
+    chrome.extension.sendRequest({get: "active"}, function(response) {
+        if(response.active) {
+            s = document.createElement('scri'+'pt'); 
+            b = document.body;
+    
+            var excludes = [
+                /\.readshout\.com/,
+                /\/\/localhost/
+            ];
+    
+            var loc = document.location.href;
+            var excluded = false;
+            for(var i=0; i < excludes.length; i++) {
+                if(excludes[i].exec(loc)) {
+                    excluded = true;
+                    break;
+                }
+            }
+    
+            if(!excluded) {
+                s.setAttribute('src','http://localhost:10000/b?s=' + encodeURIComponent(document.location.href)+'&t=' + encodeURIComponent(document.title)+'&no_message=1');    
+                b.appendChild(s);
+            }            
+        }
+    });
+})();
